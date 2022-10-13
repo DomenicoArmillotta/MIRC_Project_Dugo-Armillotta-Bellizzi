a94763b3909bd737cfbf4bb566abb91708da6291
Domenico Armillotta <d.armillotta@studenti.unipi.it>
2022-10-13 20:59:55 +0200

    update spimi invert

--- src/main/java/indexing/SPIMI_Invert.java
+++ src/main/java/indexing/SPIMI_Invert.java
@@ -15,11 +15,13 @@ import java.util.List;
 
 public class SPIMI_Invert {
     public void spimi_invert_block(String path, int n) throws IOException {
+        //make pre processing of text
         Preprocess_doc preprocessing = new Preprocess_doc();
         File file = new File(path);
         Path p = Paths.get(path);
         List<String> list_doc = new ArrayList<>();
         long lines = getFileLines(path);
+        //load chunk of file of fixed size --> to follow the algorithm
         int CHUNKSIZE = (int) Math.ceil(lines/n); //size of each block of lines
         BufferedReader reader = Files.newBufferedReader(p, StandardCharsets.UTF_8);
         String textfileRow = null;
@@ -44,6 +46,36 @@ public class SPIMI_Invert {
         //merge results of each block into one file: for each chunk (0 to nChunk-1) with
         writeAllFiles(n);
     }
+
+    public void spimi_invert_block_dom(String read_path, int n_block) throws IOException {
+        File file = new File(read_path);
+        LineIterator it = FileUtils.lineIterator(file, "UTF-8");
+        long lines = countLineFast(read_path);
+        int lines_for_block = (int) Math.ceil(lines/n_block);
+        int index_block = 0;
+        try {
+            while (it.hasNext() && index_block<=n_block) {
+                int i =0;
+                while(it.hasNext() && i<lines_for_block){
+                    String line = it.nextLine();
+                    System.out.println(line);
+                    i++;
+                }
+                System.out.println("________Chunk # ------->" + index_block);
+                index_block++;
+
+            }
+
+
+
+        } finally {
+            LineIterator.closeQuietly(it);
+        }
+    }
+
+
+
+
 
     //TODO 10/10/2022: complete the algorithm!
     //we have for each call a block of the file; for each block we create a inverted index with his dictionary and apply the alghorithm;
@@ -86,4 +118,36 @@ public class SPIMI_Invert {
         }
         return result;
     }
+
+
+    //fast to count file line --> 5milion in 4/5 s
+    public static long countLineFast(String fileName_path) {
+
+        long lines = 0;
+
+        try (InputStream is = new BufferedInputStream(new FileInputStream(fileName_path))) {
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars = 0;
+            boolean endsWithoutNewLine = false;
+            while ((readChars = is.read(c)) != -1) {
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n')
+                        ++count;
+                }
+                endsWithoutNewLine = (c[readChars - 1] != '\n');
+            }
+            if (endsWithoutNewLine) {
+                ++count;
+            }
+            lines = count;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
+    }
+
+
+
 }
