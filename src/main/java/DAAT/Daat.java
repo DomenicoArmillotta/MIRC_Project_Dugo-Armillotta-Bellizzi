1b2bd2a12c7603b7d18f87584b14388a6c9fa40c
Stepnod98 <stefanodugo1998@gmail.com>
2022-10-24 10:59:16 +0200

    corrected tfidf formula

--- src/main/java/DAAT/Daat.java
+++ src/main/java/DAAT/Daat.java
@@ -355,7 +355,7 @@ public class Daat {
     private double tfidf(int tf_q, int tf_d, int d_len, int q_len, int doc_freq){
         System.out.println(tf_q + " " + tf_d + " "  + d_len + " " + q_len + " " + doc_freq);
         double factor1 = ((double)tf_q/q_len);
-        double factor2 = (double)(tf_d*ht_docindex.keySet().size()/doc_freq)/(double)d_len;
+        double factor2 = ((double)tf_d*Math.log(ht_docindex.keySet().size()/doc_freq))/(double)d_len;
         return factor1*factor2;
     }
 }
